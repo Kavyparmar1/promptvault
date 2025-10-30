e8091903f98a79bf9315a13d73afaee999449971
kavyparmar1 <kavyparmar2005@gmail.com>
2025-10-30 18:51:40 +0530

    react dash done

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dash from '../components/Dash'
+
+const Dashboard = () => {
+  return (
+    <div className='h-screen w-full bg-[#5D6253] px-20 flex items-center justify-between'>
+        <Dash />
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          dashFont: ['dashFont', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
